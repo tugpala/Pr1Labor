f5bb962dc29a644a2397f017a1bb135f517fcbfa
tugpala <44192956+tugpala@users.noreply.github.com>
2018-10-16 11:03:42 +0200

    Update HelloWorld.java

--- uebung1/src/uebung1/HelloWorld.java
+++ uebung1/src/uebung1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Automatisch generierter Methodenstub
         System.out.println("Hello World");
+	System.out.println("Hello Germany");
 	}
 
 }
